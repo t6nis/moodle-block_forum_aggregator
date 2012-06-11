5514118ae4a5444f37020202f067e8ce2a836e9d
t6nis <t6nis20@gmail.com>
2012-06-11 21:35:04 +0300

    forum aggregator init

---
+++ lang/en/block_forum_aggregator.php
@@ -0,0 +1,19 @@
+<?php
+$string['pluginname'] = 'Forum Aggregator';
+$string['forum_aggregator'] = 'Forum Aggregator';
+$string['blocktitle'] = 'Forum Aggregator';
+
+$string['configtitle'] = 'Block title';
+$string['forum_selection'] = 'Forum selection';
+$string['noposts'] = 'No posts';
+
+$string['posts'] = 'Posts';
+
+//Help strings
+$string['max_num_of_posts'] = 'Maximum number of posts';
+$string['max_num_of_posts_help'] = 'Select maximum number of latest posts to be shown in block.';
+
+$string['forum_selection'] = 'Forum selection';
+$string['forum_selection_help'] = 'If you want to show posts from this specific forum, then you have to check this box.'
+
+?>
