9808f04b7ba0f48b46a21a09905cb89cbe779615
T6nis <t6nis20@gmail.com>
2013-05-06 11:19:03 +0300

    update version.php

--- version.php
+++ version.php
@@ -21,8 +21,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version = 2013050600;
+$plugin->version = 2013050601;
 $plugin->requires = 2010112400;
 
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "2013050600"; // User-friendly version number
+$plugin->release   = "2013050601"; // User-friendly version number
